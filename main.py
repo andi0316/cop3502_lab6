d2ca48a0c8eb461cd3a920e622d1545e63bd5c5b
andi0316 <andriasubhit@gmail.com>
2023-10-31 13:53:40 -0400

    Added comments.

--- main.py
+++ main.py
@@ -3,11 +3,11 @@
 #10-24-23
 
 def encoder(password):
-    encoded_str = ""
+    encoded_str = ""    # will hold the new, updated string
 
     translator = {'0': '3', '1': '4', '2': '5', '3': '6', '4': '7', '5': '8', '6': '9', '7': '0', '8': '1', '9': '2'}
 
-    for i in password:
+    for i in password:  # for every character in the string, the dictionary will give its translated equivalent
         temp = translator[i]
         encoded_str += temp
 
@@ -22,7 +22,9 @@ def main():
 
         if choice == 1:
             password = input("Please enter your password to encode: ")
-            storage = encoder(password)
+            storage = encoder(password)     # stores the translated password to be called upon later
 
             print("Your password has been encoded and stored!\n")
 
+
+main()
