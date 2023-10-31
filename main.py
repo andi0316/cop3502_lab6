0f0d1324440f1fb289ab451820fa91965df444bf
anthonyle1 <leanthony2284@gmail.com>
2023-10-31 15:46:31 -0400

    added decoder() function, completed menu -> main() function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-#Andria Subhit
+#Andria Subhit (Owner), Anthony Le
 #Lab 6
 #10-24-23
 
+# Andria
 def encoder(password):
     encoded_str = ""    # will hold the new, updated string
 
@@ -13,7 +14,23 @@ def encoder(password):
 
     return encoded_str
 
+# Anthony
+def decoder(encrypted_password):
+    # dictionary used to subtract three (or add 7 if less than 2);
+    # essentially reverts changes made from encoder() function
+    translator_2 = {'0': '7', '1': '8', '2': '9', '3': '0', '4': '1', '5': '2', '6': '3', '7': '4', '8': '5', '9': '6'}
+    decoded_str = ""
+
+    # for every digit in the encrypted password,
+    for i in encrypted_password:
+        temp = translator_2[i]
+        decoded_str += temp
+
+    return decoded_str
+
+
 def main():
+    # Andria
     menu_repeats = True
     while menu_repeats:
         print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
@@ -26,5 +43,15 @@ def main():
 
             print("Your password has been encoded and stored!\n")
 
+        # Anthony
+        elif choice == 2:
+            # decrypts encrytped string, then shows both encrypted and unencrypted passwords to the user.
+            decoded_str = decoder(storage)
+            print(f"The encoded password is {storage}, and the original password is {decoded_str}.")
 
-main()
+        # Andria
+        elif choice == 3:
+            menu_repeats = False # stops program
+
+if __name__ == "__main__":
+    main()
